acbb05f65bdba9ae3d39be72626bbc11c5b01f44
Danny Chen <dannyisadog10@gmail.com>
2025-06-12 23:13:33 +0800

    feat(robots): add robots.txt generation for SEO with user agent rules

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://york-tsai.vercel.app/sitemap.xml',
+  }
+} 
